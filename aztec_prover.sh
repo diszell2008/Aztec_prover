e1da97d15da8d9b63b1af892e7ecac7862a61145
diszell2008 <listening2009@gmail.com>
2025-08-04 07:45:02 +0700

    Update 1.2.0

--- aztec_prover.sh
+++ aztec_prover.sh
@@ -5,7 +5,7 @@
 # ======================
 
 # ---------- Defaults ----------
-IMAGE="aztecprotocol/aztec:1.1.2"
+IMAGE="aztecprotocol/aztec:1.2.0"
 NETWORK="alpha-testnet"
 DATA_DIR="/home/diszell2008/aztec-prover"
 P2P_PORT="40400"
